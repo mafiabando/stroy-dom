16ee7cb6a4341de930bf4c0d5ae2bb09c8b5e0f5
PC Slavyanka <killa265montana@icloud.com>
2025-08-11 11:38:57 +0400

    yandex commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { YMInitializer } from 'react-yandex-metrika';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Contacts from './pages/Contacts/Contacts';
@@ -6,13 +7,40 @@ import Cart from './pages/Cart/Cart';
 import { CartProvider } from './context/CartContext';
 import { ModalProvider } from './context/ModalContext';
 import PrivacyPolicy from './pages/Privacy/PrivacyPolicy';
+import { useEffect } from 'react';
+
+const METRIKA_ID = 103535711;
+
+const RouteChangeTracker = () => {
+  const location = useLocation();
+  const ym = window.ym; // Получаем экземпляр ym из глобальной области видимости
+
+  useEffect(() => {
+    if (window.ym) {
+      console.log("Яндекс.Метрика (ym) найдена!");
+      window.ym(METRIKA_ID, 'hit', location.pathname + location.hash);
+    } else {
+      console.warn("Яндекс.Метрика (ym) не найдена!");
+    }
+  }, [location]);
+
+  return null; // Этот компонент ничего не рендерит
+};
 
 const App = () => {
   return (
     <ModalProvider>
     <CartProvider>
     <HashRouter> 
+      <YMInitializer accounts={[METRIKA_ID]} options={{ 
+            clickmap:true, 
+            trackLinks:true, 
+            accurateTrackBounce:true, 
+            webvisor: true,
+            trackHash: true // Важно для HashRouter
+          }} version="2" />
       <Header />
+      <RouteChangeTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:slug" element={<Home />} />
